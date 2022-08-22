e21d7320e8b2eabb1d904b512066a4812990abb3
Larkspurrr <111087667+Larkspurrr@users.noreply.github.com>
2022-08-21 21:16:27 -0600

    Update main.js
    Changed "timer" to "stopwatch" in code.

--- main.js
+++ main.js
@@ -1,18 +1,13 @@
-let Start = document.getElementById("Start");
-let Stop = document.getElementById("Stop");
-let Reset = document.getElementById("Reset");
-let timer = document.getElementById("timer");
+let buttons = Array.from(document.getElementsByClassName("button"));
+let stopwatch = document.getElementById("stopwatch");
 
-let seconds = 00;
-let minutes = 00;
-let hours = 00;
+let seconds = 0, minutes = 0, hours = 0;
 
 let active = false;
 
-let turnOnTimer;
-let isClickable;
+let turnOnstopwatch, isClickable;
 
-function activeTimer() {
+function activestopwatch() {
     if (active == true) {
         seconds += 1;
         if (seconds == 60) {
@@ -23,28 +18,28 @@ function activeTimer() {
             minutes = 00;
             hours += 1;
         }
-        timer.innerText = hours + " : " + minutes + " : " + seconds;
+        stopwatch.innerText = hours + " : " + minutes + " : " + seconds;
     }
 }
 
-Start.addEventListener("click", () => {
-    active = true;
-    isClickable = Start.style.pointerEvents = "none";
-    turnOnTimer = setInterval(activeTimer, 1000);
-})
-
-Stop.addEventListener("click", () => {
-    active = false;
-    isClickable = Start.style.pointerEvents = "auto";
-    clearInterval(turnOnTimer);
-})
-
-Reset.addEventListener("click", () => {
-    isClickable = Start.style.pointerEvents = "auto";
-    active = false;
-    clearInterval(turnOnTimer);
-    seconds = 00;
-    minutes = 00;
-    hours = 00;
-    timer.innerText = "0 : 0 : 0";
+buttons.map(button => {
+    button.addEventListener("click", (e) => {
+        switch(e.target.innerText) {
+            case "Start":
+                active = true;
+                isClickable = Start.style.pointerEvents = "none";
+                turnOnstopwatch = setInterval(activestopwatch, 1000);
+                break;
+            case "Reset":
+                seconds = 0;
+                minutes = 0;
+                hours = 0;
+                stopwatch.innerText = "0 : 0 : 0";
+            default:
+                active = false;
+                isClickable = Start.style.pointerEvents = "auto";
+                clearInterval(turnOnstopwatch);
+                
+        }
+    })
 })
